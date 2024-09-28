ee342f90efcf81cfe716581829cd29795f9601f9
bwilmer <bwilmer@redcloud.uccs.edu>
2024-09-28 11:56:35 -0600

    Added to client file

--- project-one/udp_client.c
+++ project-one/udp_client.c
@@ -27,104 +27,118 @@
 *CLOSE - Release the connection
 */
 
-#define SERVER_PORT 12345 //Arbitrary server number
+#define SERVER_PORT blahblahblah //Arbitrary server number
 #define BUF_SIZE 4096 //Block transfer size
 
-int main(int argc, char **argv) {
-	//Variable declarations
-	int c;	
-	int s;		
-	int bytes;	
-	char buf[BUF_SIZE];		//Buffer for incoming file
-	struct hostent *h;		//Info from server
-	struct sockaddr_in channel;	//Holds IP address
+int main(int argc, char** argv) {
+	//Define variables
+	int bytes;		    //Size of file from server
+	char buffer[BUF_SIZE];	    //Buffer for file from server
+	struct hostent *hostInfo;   //Info about server
+	struct sockaddr_in channel; //Holds IP address
 
-	//Get info from the server
-	if(argc != 3) fatal("Usage: client server-name file-name");
-	h = gethostbyname(argv[1]);		//Look up host's IP adddress
-	if (!h) fatal("gethostbynamefailed");
-
-	//Create a socket that connects to the server side socket
-	createSocket();	
-
-	//Attempt to connect to the server
-	connection = connect(s, (struct sockaddr *) &channel, sizeof(channel));
-	if (connection < 0) fatal("Connect failed");
-
-	//Connection is established, send file name to server
-	write(s, argv[2], strlen(argv[2])+1);
-
-	//Get the file and write it to stdout
-	while(1) {
-		bytes = read(s, buf, BUF_SIZE);	//Read from server socket
-		if (bytes <= 0) exit(0);	//Check for end of file
-		write(1, buf, bytes);		//Write to stdout
-	}
-
-	//Call ARQType function to determine the ARQ algorithm to be used
-	protocol = ARQType(arqNumber);
-
-	//Perform either Stop and Wait or Go Back N Protocol
-	if (protocol.equals("Stop and Wait")) {
-		StopAndWait();
-	else if (protocol.equals("GoBackN")) {
-		GoBackN();
-	else {
-		perror("No protocol specified");
-	}
-
+	int clientSocket;
+	char fileName[100];
+	
+	getHostInfo()
+	clientSocket = createSocket(*hostInfo, *channel);
+	serverConnect(clientSocket, *hostInfo, *channel);
+	fileName = selectFile();
+	requestFile(*fileName, clientSocket **argv, bytes, *buffer);	
+	
 }//main
 
+//Function to get the host info
+void getHostInfo() {
 
-//Function to create UDP socket
-void createSocket() {
-	//Create a socket that connects to the server side socket
-        clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+}//getHostInfo
 
-	//Print error if socket could not be made
-        if(clientSocket < 0) {
-		perror("Socket could not be created");
+//Function to create an UDP socket
+int createSocket(struct hostent *hostInfo, struct sockaddr_in *channel) {
+	//Create the socket
+	int clientSocket;
+	clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	//Check to make sure socket was made
+	if (clientSocket < 0) {
+		perror("Socket could not be created.");
 	}
+	
+	memset(&channel, 0, sizeof(channel)); 	//Set IP address to 0
+	channel.sin_family = AF_INET;		//Use IPV4		
+	memcpy(&channel.sin_addr.s_addr, hostInfo->h_addr, hostInfo->h_length);	//Copy host info to IP address
+	channel.sin_port_htons(SERVER_PORT);	//Set IP address port to the server port
 
-	//Link the socket to the specified server port
-        memset(&channel, 0, sizeof(channel));
-        channel.sin_family = AF_INET;
-        memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
-        channel.sin_port=htons(SERVER_PORT);
+	//Return socket
+	return clientSocket;
 }//createSocket
 
-//Stop and Wait function (SW)
+//Function to connect the client to the server
+void serverConnect(int clientSocket, struct hostent *hostInfo, struct sockaddr_in *channel) {
+	//Attempt to create the connection
+	int connection;
+	connection = connect(clientSocket, (struct sockaddr *) &channel, sizeof(channel));
+
+	//Print error if connection failed
+	if (connection < 0) {
+		perror("Connection failed.");
+	}
+}//serverConnect
+
+//Function to determine parameter protocol type (ARQ type) 1 for SW 2 for GBN
+String ARQType(int arqNumber) {
+        if(arqNumber == 1) {
+                return "Stop and Wait";
+        else if(arqNumber == 2) {
+                return "Go Back N";
+        else {
+                perror("Unidentified ARQ protocol.")
+}//ARQType
+
+//Function to select a file name to receive from the server
+char selectFile() {
+	char fileName[100];
+	
+	printf("Please enter the name of the file you want to request:");
+	if (fgets(fileName, sizeof(fileName), stdin) != NULL) {
+		printf("You are requesting: %s", fileName);
+	}
+	else {
+		perror("Could not read file name.");
+	}
+
+	return fileName;
+}//selectFile
+
+//Function to request the file
+void requestFile(char *fileName, int clientSocket, char **argv, int bytes, char *buffer) {
+	//Send file name to server
+	write(clientSocket, argv[2], strlen(argv[2])+1);
+
+	//After receiving the file, write to standard out
+	int readingFile = 1;
+	while(readingFile) {
+		bytes = read(clientSocket, buffer, BUF_SIZE);	//Read from socket
+		if (bytes <= 0) {				//Check for end of file
+			exit(0);
+		}
+		write(1, buffer, bytes);			//Write to standard out
+	}
+}//requestFile
+
+//Function for Stop and Wait (SW)
 void StopAndWait () {
 	//Receive packets one by one
 	//Simulate packet loss from server	
 }/StopAndWait
 
-//Go Back N function (GBN)
+//Function for Go Back N (GBN)
 void GoBackN() {
 	//Receive packets in blocks
 	//Simulate packet loss from server
 }//GoBackN
 
-//Recieve dropping probability from the server
-
-
-//Determine parameter protocol type (ARQ type) 1 for SW 2 for GBN
-String ARQType(int arqNumber) {
-	if(arqNumber == 1) {
-		return "Stop and Wait";
-	else if(arqNumber == 2) {
-		return "Go Back N";
-	else {
-		perror("Unidentified protocol type entered.");
-}//ARQType
-
-
-//Send file name to server
-
-//Close socket when finished
-close(socket);
-
-//Error handling
+//Function to perform error handling
 perror(char *string) {
 	printf("%s\n%, string);
 	exit(1);
